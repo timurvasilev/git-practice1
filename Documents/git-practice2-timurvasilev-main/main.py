ef73477cd8ee470eaf4added19518d6a6ab3b4f6
timurvasilev <timurvasilev9@gmail.com>
2025-05-22 14:09:32 +0900

    <Написан код внутри функции nod()>

---
+++ Documents/git-practice2-timurvasilev-main/main.py
@@ -0,0 +1,16 @@
+
+def nod(a, b):
+    # напишите ваше решение здесь
+    return 0
+
+
+def nok(a, b):
+    # напишите ваше решение здесь
+    return 0
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(nod(a, b))
+    print(nok(a, b))
